18372e8e42a7cff776aeefd461d1b828a2e6cf22
unknown <319344799@qq.com>
2019-11-08 21:47:57 +0800

    myoriject

---
+++ src/components/text.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="text" v-for="(item,index) in test" :key="index">{{item}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    test: {
+      type: Array,
+      default(){
+          return []
+      }
+    }
+  },
+  data() {
+    return {
+      msg: 5555
+    };
+  }
+};
+</script>
+<style scoped>
+.text {
+  height: 200px;
+  width: 200px;
+  background-color: pink;
+}
+</style>
